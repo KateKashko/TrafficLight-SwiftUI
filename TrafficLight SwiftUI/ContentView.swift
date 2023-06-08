8fe24c6a1b952b3ec4d1f47c861df01137853643
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-06-08 19:10:15 +0300

    Add views

--- TrafficLight SwiftUI/ContentView.swift
+++ TrafficLight SwiftUI/ContentView.swift
@@ -10,15 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            LightView(color: .red, figure: "figure.stand")
+            LightView(color: .yellow, figure: "")
+            LightView(color: .green, figure: "figure.walk")
+            Spacer()
+            Button(action: {}) {
+                
+                Text("Next")
+                    .font(.largeTitle)
+                    .frame(width: 150.0, height: 50.0)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            
         }
-        .padding()
+        .padding(.top, 40)
+//        private func nextColor() {
+//
+//        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ TrafficLight SwiftUI/LightView.swift
@@ -0,0 +1,32 @@
+//
+//  CircleView.swift
+//  TrafficLight SwiftUI
+//
+//  Created by Kate Kashko on 8.06.2023.
+//
+
+import SwiftUI
+
+struct LightView: View {
+    let color: Color
+    let figure: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .frame(width: 140, height: 140)
+                .cornerRadius(20)
+            Circle()
+                .frame(width: 120)
+                .foregroundColor(color)
+            Image(systemName: figure)
+                .font(.system(size: 70))
+        }
+    }
+}
+
+struct LightView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightView(color: .blue, figure: "figure.stand")
+    }
+}
